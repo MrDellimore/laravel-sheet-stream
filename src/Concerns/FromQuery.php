c0c2214afee30a83743e2b60229bb7ba64472837
Andrew M. Dellimore <andrewdellimore@gmail.com>
2026-09-02 09:40:50 -0400

    Expand `FromQuery` interface to support `Relation` and `QueryBuilder` types alongside `EloquentBuilder`.

--- src/Concerns/FromQuery.php
+++ src/Concerns/FromQuery.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace MrDellimore\SheetStream\Concerns;
 
-use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 interface FromQuery
 {
-    public function query(): Builder;
+    public function query(): EloquentBuilder|QueryBuilder|Relation;
 }
